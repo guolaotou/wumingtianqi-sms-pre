c74f743af7922820da7de4010f843212e74b1629
guolaotou <guoyu_137996643@outlook.com>
2020-10-03 17:44:18 +0800

    邀请码脚本完成

--- scripts/invitation/invitation.go
+++ scripts/invitation/invitation.go
@@ -7,48 +7,106 @@ import (
 	"fmt"
 	"github.com/satori/go.uuid"
 	"math/rand"
-	"reflect"
 	"time"
 	"wumingtianqi/config"
+	"wumingtianqi/model"
+	"wumingtianqi/model/user"
+	"wumingtianqi/utils"
 )
 
+/**
+ * @Author Evan
+ * @Description 生成邀请码
+	step1: 生成uuid
+	step2：该uuid随机取32位（可重复取）进行md5加密
+	step3: uuid随机取16位（不重复），md5加密后的字符串随机取16位（不重复），然后拼接
+	注：(uuid参考 https://github.com/satori/go.uuid)
+ * @Date 12:57 2020-10-03
+ * @Param
+ * @return string 最终生成的邀请码
+ **/
 func generateInvitationCode() string {
-	/* 生成邀请码
-	step1: 生成uuid (uuid参考 https://github.com/satori/go.uuid)
-	step2: 用
-	 */
-
 	// step1: 生成uuid
 	u1 := uuid.NewV4()
-	u1Str := u1.String()
-	fmt.Printf("UUIDv4: %s\n", u1Str)
+	strUUID36 := u1.String()
+	//fmt.Printf("UUIDv4: %s\n", strUUID36)
 
 	// step2：该uuid随机取32位（可重复取）进行md5加密
 	strUUID32 := ""
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	println("duandian1", u1Str)
 	for i := 0; i < 32; i++ {
-		index := r.Intn(36)
-		strUUID32 += string(u1Str[index])
+		index := r.Intn(36)  // uuid长度
+		strUUID32 += string(strUUID36[index])
 	}
-	key := config.GlobalConfig.Util.Uuid2MD5Key
-	println("key", key)
-	md5Res, _ := MD5(strUUID32, key)
-	println("md5", md5Res)
-	println("r", r)
+	key := config.GlobalConfig.Util.Uuid2MD5Key  // 加盐
+	strMd532, _ := MD5(strUUID32, key)
 
-	fmt.Println("r", reflect.TypeOf(r))
-	resList := randomNM(0, 1, r)
-
-	for i := 0; i < len(resList); i++ {
-		println("i: ", resList[i])
+	// step3: uuid随机取16位（不重复），md5加密后的字符串随机取16位（不重复），然后拼接
+	uuidIndexList := randomNM(35, 16, r)
+	md5IndexList := randomNM(31, 16, r)
+	strUUID16 := ""
+	strMd516 := ""
+	for i := 0; i < 16; i++ {
+		strUUID16 += string(strUUID36[uuidIndexList[i]])
+		strMd516 += string(strMd532[md5IndexList[i]])
 	}
+	// 预拼接，然后从0~31之间选一个位置做头
+	strTemp64 := strUUID16 + strMd516 + strUUID16 + strMd516
+	index := r.Intn(32)
 
-	// step3: uuid随机取16位，md5加密后的字符串随机取16位，然后拼接
+	// 拼接
+	strInvitation32 := strTemp64[index:index+32]
+	//fmt.Println("strInvitation32", strInvitation32)
 
-	// step4: 为该邀请码配置权限，并把该邀请码存入数据库
+	return strInvitation32
+}
 
-	return u1Str
+/**
+ * @Author Evan
+ * @Description 给邀请码分配权限——一等邀请码
+	（mine生成10个）VIP3，有效期99年，初始10000个钻石
+ * @Date 13:17 2020-10-03
+ * @Param
+ * @return
+ **/
+func SetInvitationCodeAuthLevel1(invitationCode string) *user.Invitation {
+	invitationModel := new(user.Invitation)
+	currentTime := time.Now()
+	invitationModel.InvitationCode = invitationCode
+	invitationModel.TimesMax  = 1
+	invitationModel.TimesRemaining  = 1
+	invitationModel.Vip = utils.VIP3
+	invitationModel.Duration = 366 * 99
+	invitationModel.Coin = 0
+	invitationModel.Diamond = 10000
+	invitationModel.Creator = -1
+	invitationModel.CreateTime = currentTime
+	invitationModel.UpdateTime = currentTime
+	return invitationModel
+}
+
+/**
+ * @Author Evan
+ * @Description 给邀请码分配权限——二等邀请码
+	mine生成5个，每个可以给20个人用） 默认VIP2，有效期99年，默认100钻石
+ * @Date 17:18 2020-10-03
+ * @Param
+ * @return
+ **/
+func SetInvitationCodeAuthLevel2(invitationCode string) *user.Invitation {
+	invitationModel := new(user.Invitation)
+	currentTime := time.Now()
+	invitationModel.InvitationCode = invitationCode
+	invitationModel.TimesMax = 20
+	invitationModel.TimesRemaining = 20
+	invitationModel.Vip = utils.VIP2
+	invitationModel.Duration = 366 * 99
+	invitationModel.Coin = 0
+	invitationModel.Diamond = 100
+	invitationModel.Creator = -1
+	invitationModel.CreateTime = currentTime
+	invitationModel.UpdateTime = currentTime
+	return invitationModel
 }
 /**
  * @Author Evan
@@ -125,9 +183,43 @@ func MD5(originStr string, key string) (string, error) {
 	ctx := md5.New()
 	ctx.Write([]byte(text))
 	res := hex.EncodeToString(ctx.Sum(nil))
-	println("resdfgh", res)
 	return res, nil
 }
+
+/**
+ * @Author Evan
+ * @Description 全流程：生成10个一等邀请码，5个二等邀请码;为该邀请码配置权限，并把该邀请码存入数据库
+ * @Date 17:20 2020-10-03
+ * @Param
+ * @return
+ **/
+func ProcessAll() {
+	model.InitMysql()
+	//session := common.Engine.NewSession()
+	//defer session.Close()
+	// 1. 生成10个一等邀请码，分别为邀请码配置权限，并把该邀请码存入数据库
+	for i := 0; i < 10; i++ {
+		invitationCode := generateInvitationCode()
+		invitationModel := SetInvitationCodeAuthLevel1(invitationCode)
+		if err := invitationModel.Create(); err != nil {
+			println("Creating level 1")
+			panic(err)
+		}
+	}
+
+	// 2. 生成5个二等邀请码，分别为邀请码配置权限，并把该邀请码存入数据库
+	for i := 0; i < 5; i++ {
+		invitationCode := generateInvitationCode()
+		invitationModel := SetInvitationCodeAuthLevel2(invitationCode)
+		if err := invitationModel.Create(); err != nil {
+			println("Creating level 2")
+			panic(err)
+		}
+	}
+	// alter table invitation AUTO_INCREMENT=1;
+}
+
+
 
 
 // go run scripts/invitation/invitation.go
@@ -138,7 +230,11 @@ func main() {
 	if _, err := config.LoadConfig(); err != nil {
 		fmt.Println(err.Error())
 	}
-	generateInvitationCode()
 
+	// 生成邀请码
+	strInvitation32 := generateInvitationCode()
+	println("strInvitation32", strInvitation32)
 
+	// 全流程：生成10个一等邀请码，5个二等邀请码;为该邀请码配置权限，并把该邀请码存入数据库
+	ProcessAll()
 }
--- utils/const_define.go
+++ utils/const_define.go
@@ -8,3 +8,16 @@ const (
 	SmsOneGoroutine               = 10 * time.Second
 	PrepareSmsContentGoroutineNum = 10
 )
+
+const (
+	VIP1 = 1
+	VIP2 = 2
+	VIP3 = 3
+	VIPMaster = -1
+)
+
+const (
+	InvitationLevel1 = 1  // 一等邀请码（mine生成10个）VIP3，有效期99年，初始10000个钻石
+	InvitationLevel2 = 2  // 二等邀请码（mine生成5个，每个可以给20个人用） 默认VIP2，有效期99年，默认100钻石
+	InvitationLevel3 = 3
+)
