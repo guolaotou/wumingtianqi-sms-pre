0db7e599c7cb5f2e4df7890a44d57fd990106df6
guolaotou <guoyu_137996643@outlook.com>
2021-01-15 20:18:05 +0800

    bug修复：token不正确后应该直接abort

--- router/middleware/header.go
+++ router/middleware/header.go
@@ -8,6 +8,8 @@ import (
 	"wumingtianqi/utils/errnum"
 )
 
+// token有问题直接返回 context.Abort https://blog.csdn.net/minibrid/article/details/95354754
+// google关键词: gin 终止中间件返回数据
 /**
  * @Author Evan
  * @Description Token is a middleware function that convert token to user_id
@@ -21,11 +23,13 @@ func TokenParsing(context *gin.Context) {
 	if userInfoModel, has, err := userInfoModelInstance.QueryByUserToken(token); err != nil {
 		err := errnum.New(errnum.DbError, err)
 		handler.SendResponse(context, err, nil)
+		context.Abort()
 		return
 	} else if !has {
 		err = errnum.New(errnum.ErrTokenNotExist, nil)
 		// todo log
 		handler.SendResponse(context, err, nil)
+		context.Abort()
 		return
 	} else {
 		context.Request.Header.Add("X-User-Id", strconv.Itoa(userInfoModel.Id))
